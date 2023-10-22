89aa7762ff76f7b952aebc46d8429c9a0453ebc9
Jai Nijhawan <44671239+jainijhawan@users.noreply.github.com>
2023-10-22 14:53:07 -0400

    Completed "Users can grant location access to the app"

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
     }
-
-
+    
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined,
+                .restricted,
+                .denied:
+            break
+            
+        case .authorizedAlways,
+                .authorizedWhenInUse,
+                .authorized:
+            guard let locValue = manager.location?.coordinate else { return }
+            print("locations = \(locValue.latitude) \(locValue.longitude)")
+        @unknown default:
+            break
+        }
+    }
+}
